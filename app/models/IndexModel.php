ef3814be22484c66f358adeba7f6b782836a8eaa
sojebsikder <sojebsikder@gmail.com>
2020-07-20 16:59:16 +0600

    uploaded

---
+++ app/models/IndexModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class IndexModel extends Model{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function dataList($query){
+        return $this->db->select($query);
+    }
+}
+
+
+?>
