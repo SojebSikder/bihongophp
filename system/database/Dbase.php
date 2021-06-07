22ea7dc5ed1313d8f6094329d2eea75e39f37495
sojebsikder <sojebsikder@gmail.com>
2021-06-07 17:32:23 +0600

    main

--- system/database/Dbase.php
+++ system/database/Dbase.php
@@ -1,32 +1,37 @@
 <?php
 //include "MySQLAdapter.php";
 //namespace DB;
-Class Dbase{
+class Dbase
+{
 
   protected $adapter;
- 
-  public function __construct(AdapterInterface $adapter){
+
+  public function __construct(AdapterInterface $adapter)
+  {
     $this->adapter = $adapter;
   }
- 
+
   // Select or Read data
-  public function select($query){
-   return $this->adapter->select($query);
+  public function select($query)
+  {
+    return $this->adapter->select($query);
   }
-  
+
   // Insert data
-  public function insert($query){
+  public function insert($query)
+  {
     return $this->adapter->insert($query);
   }
-    
+
   // Update data
-  public function update($query){
+  public function update($query)
+  {
     return $this->adapter->update($query);
   }
-    
+
   // Delete data
-  public function delete($query){
+  public function delete($query)
+  {
     return $this->adapter->delete($query);
   }
- 
 }
--- system/database/drivers/AdapterInterface.php
+++ system/database/drivers/AdapterInterface.php
@@ -1,10 +1,10 @@
 <?php
 //namespace DB;
-interface AdapterInterface{
+interface AdapterInterface
+{
 
   public function select($query);
   public function insert($query);
   public function update($query);
   public function delete($query);
- 
 }
