e9249cb86b3dbba50837a9e4f673b55854b675cd
sojebsikder <sojebsikder@gmail.com>
2021-06-13 23:17:42 +0600

    main

--- system/core/Response.php
+++ system/core/Response.php
@@ -3,12 +3,18 @@
 
 namespace System\Core;
 
+use System\Helpers\ArrayHelper;
 
 /**
  * Class Response
  */
 class Response
 {
+
+    public function json($data)
+    {
+        return ArrayHelper::json($data);
+    }
     public function statusCode(int $code)
     {
         http_response_code($code);
