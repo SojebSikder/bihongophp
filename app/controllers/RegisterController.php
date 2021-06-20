17f02b25287558f4d0f0f9ae39080230e67bc98e
sojebsikder <sojebsikder@gmail.com>
2021-06-20 15:02:19 +0600

    main

---
+++ app/controllers/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\Controller;
+use System\Core\Request;
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function register(Request $request)
+    {
+
+        $userModel = model("Register");
+
+        if ($request->input("submit")) {
+            $username =  xss($request->input("username"));
+            $email = xss($request->input("email"));
+            $password = password_hash(xss($request->input("password")), PASSWORD_DEFAULT);
+
+            $exe = $userModel->register($username, $email, $password);
+
+            if ($exe) {
+                return response()->redirect("/login");
+            }
+        }
+
+        return view("register.te");
+    }
+
+    public function login(Request $request)
+    {
+        $userModel = model("Register");
+
+        if ($request->input("submit")) {
+            $username = xss($request->input("username"));
+            $password = xss($request->input("password"));
+
+            $exe = $userModel->login($username, $password);
+
+            echo "Login successfully";
+            return response()->redirect("/home");
+        }
+
+        return view("login.te");
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        return response()->redirect("/home");
+    }
+}
---
+++ resources/views/register.te.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    {{{ BASE }}}
+    <meta charset="UTF-8">
+    <link rel="icon" href="" type="image/png" sizes="16x16">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to {{ name }}</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/material.css') }}">
+
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jsoj.js') }}"></script>
+    <script src="{{ asset('assets/js/material.js') }}"></script>
+</head>
+
+<body>
+
+
+    <div class="container">
+        <div class="m-justify">
+            <div class="m-card">
+                <div class="m-card-body">
+
+                    <form class="form-signin" action="/register" method="post">
+
+                        <h3 class="m-center">Register</h3>
+                        <p class="m-center">It's free!</p>
+
+
+                        <div class="m-input-group">
+                            <input type="text" name="username" class="text-dark m-form-control" autofocus>
+                            <label>Name</label>
+                        </div>
+
+                        <div class="m-input-group">
+                            <input type="text" name="email" class="text-dark m-form-control">
+                            <label>Email address</label>
+                        </div>
+
+                        <div class="m-input-group">
+                            <input type="password" name="password" class="text-dark m-form-control">
+                            <label>Password</label>
+                        </div>
+
+
+                        <input class="float-left m-hidden" id="psk" type="button" ng-click="showPass()">
+                        <label class="float-left" for="psk">Show Password</label>
+
+                        <p class="float-right">Already have an account? <a href="login">Login</a></p>
+                        <input class="m-btn waves-effect m-btn-primary m-btn-block" name="submit" type="submit" value="Register">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
